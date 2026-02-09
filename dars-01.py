f55b8b68bff2f59d2753dfdbf9ce31d63d1a619e
Nurulloh <khadjayevnurullo@gmail.com>
2026-02-09 13:39:41 +0500

    dars-01.py
    O'zgaruvchilar

---
+++ dars-01.py
@@ -0,0 +1,14 @@
+
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+habar = "Salom bu yangi dastur"
+print(habar)
+
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi", radius , "ga teng aylana yuzi=", aylana_yuzi)
