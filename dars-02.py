1a54a22fb9765f904d1d652a62ff13ec03bb02e0
Nurulloh <khadjayevnurullo@gmail.com>
2026-02-13 22:05:02 +0500

    Create dars-02.py

---
+++ dars-02.py
@@ -0,0 +1,67 @@
+
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# ism = "shokir"
+# familya = "islomov"
+# yosh = 25
+# ism_familya_yosh = f"{ism} {familya} {yosh}-yoshda"
+# print("Foydalanuvchining malumotlari:", ism_familya_yosh.title())
+
+# kocha = 'bodomzor'
+# mahalla = 'obod'
+# tuman = 'oltinkol'
+# viloyat = 'andijon'
+# print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati.")
+
+# print("Iltimos quydagi malumotlardi kiriting.")
+# manzillar = []
+# kocha = input("Ko'chani kiriting: ")
+# mahalla = input("Mahallani kiriting: ")
+# tuman = input("Tumanni kiriting: ")
+# viloyat = input("Viloyatingizni kiriting: ")
+# manzillar = manzil = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati."
+# print(manzil.upper())
+# print(manzil.title())
+# print(manzil.capitalize())
+# print(manzil.lower())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
