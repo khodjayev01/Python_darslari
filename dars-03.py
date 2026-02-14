424070b3215dd30fd2a09e8e16ca25b15a2bfa7d
Nurulloh <khadjayevnurullo@gmail.com>
2026-02-14 10:58:27 +0500

    Create dars-03.py

---
+++ dars-03.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 14 10:29:57 2026
+
+@author: user
+"""
+
+# ism = 'Davron'
+# yosh = 35
+# xabar = ism + ' ' + str(yosh) + ' yoshda'
+# print(xabar)
+
+# t_yil = int(input("Tug'ilgan yilingizni kiriting: "))
+# yosh = 2026 - t_yil
+# print("Siz", yosh, "yoshdasiz.")
+
+# son = int(input("Istalgan son kiriting: "))
+# kavadrat = son**2
+# kubi = son**3
+# print(f"{son} ning kvadrati {kavadrat}ga teng \n{son} ning kubi {kubi} ga teng.")
+
+# yosh = int(input("Yoshingizni kiriting>>> "))
+# t_yil = 2026 - yosh
+# print("Siz", t_yil, "yilda tug'ilgansiz")
+
+# a = float(input("Birinchi sonni kiriting: "))
+# b = float(input("Ikkinchi sonni kiriting: "))
+# print(f"{a}+{b}=", a+b)
+# print(f"{a}-{b}=", a-b)
+# print(f"{a}*{b}=", a*b)
+# print(f"{a}/{b}=", a/b)
