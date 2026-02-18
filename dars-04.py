037710ad52a209abe740dc4f7860758a3befaa7e
Nurulloh <khadjayevnurullo@gmail.com>
2026-02-18 23:18:41 +0500

    dars-04

---
+++ dars-04.py
@@ -0,0 +1,53 @@
+
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# davlatlar = ["Uzbekistnon", "Turkiya", "Misr", "Koreya", "Singapur", "Qatar"]
+# davlatlar.reverse()
+# print(davlatlar)
+
+# davlatlar.sort(reverse=True)
+# print(davlatlar)
+
+# sonlar = list(range(120,1200,2))
+# print(max(sonlar)-min(sonlar))
+# print(len(sonlar))
+# print(sonlar[:20])
+# print(sonlar[-20:])
+# print(sonlar[200:240])
+
+# taomlar = ["osh", "shashlik", "lagmon", "kasha", "somsa"]
+# nonushta = taomlar[:]
+# nonushta.remove("osh")
+# nonushta.remove("lagmon")
+# nonushta.remove("shashlik")
+# nonushta.remove("somsa")
+# nonushta.append("non")
+# nonushta.append("saryog")
+# nonushta = tuple(nonushta)
+# print("Umumiy taomlar", taomlar)
+# print("Nonushta uchun", nonushta)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ dars-05.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 18 12:18:05 2026
+
+@author: user
+"""
+
+# ismlar = ["olim", "umar", "davron", "temur", "abdulloh"]
+# for ism in ismlar:
+#     print(f"Assalomu aleykum {ism.title()}")
+# print(f"Kod {len(ismlar)} marta takrorlandi.")
+
+# sonlar = list(range(11,101,2))
+# for son in sonlar:
+#     print(son**3)
+
+# kinolar = []
+# print('5 ta yoqtirgan kinoingiz qaysi?')
+# for kino in  range(5):
+#     kinolar.append(input(f"{kino+1}-kino "))
+# print(kinolar)
+
+# odamlar = int(input("Bugun nechta odam bilan suhbatlashtingiz: "))
+# ismlar = []
+# for n in range(odamlar):
+#     ismlar.append(input(f"{n+1}-odamni ismi nima edi>>> "))
+# print(ismlar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
