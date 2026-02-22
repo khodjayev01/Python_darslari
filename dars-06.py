77b9089822f5d5ac4462a6eab7f241ca3e6f267c
Nurulloh <khadjayevnurullo@gmail.com>
2026-02-22 12:43:28 +0500

    Create dars-06.py

---
+++ dars-06.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 19 09:44:49 2026
+
+@author: user
+"""
+
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for car in cars:
+#     if car.lower() == 'gm':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+
+# ismlar = ['sarvar', 'nodir', 'kamron', 'umar']
+# for ism in ismlar:
+#     if ism.lower() == 'umar':
+#         print(ism.upper())
+#     else:
+#         print(ism.title())
+
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for car in cars:
+#     if car.lower() != 'gm':
+#         print(car.lower())
+#     else:
+#         print(car.upper())
+
+# login = input("Salom login ismingizni yozing\n>>> ")
+# if login.lower() == "admin":
+#     print("Hush kelibsiz Admin foydalanuvchilar ro'yxatini ko'rasizmi.")
+# else:
+#     print(f"Hush kelibsiz {login.title()} foydalanuvchi.")
+
+# x = int(input("Birinchi sonni kiriting>>> "))
+# y = int(input("Ikkinchi sonni kiriting>>> "))
+# if x==y: print(f"Sonlar teng: {x}={y}")
+
+# son = int(input("Istalgan son kiriting: "))
+# if son<0:
+#     print("Manfiy son")
+# else:
+#     print("Musbat son")
+
+# son = int(input("Istalgan son kiriting: "))
+# if son>0:
+#     print(son**0.5)
+# else:
+#     print("Musbat son kiriting.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
