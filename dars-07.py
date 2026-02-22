6dae2b03d6a710fa4abcdd7781a7fc130066701e
Nurulloh <khadjayevnurullo@gmail.com>
2026-02-22 12:44:53 +0500

    Create dars-07.py

---
+++ dars-07.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 22 11:07:20 2026
+
+@author: user
+"""
+
+# son = int(input("Iltimos toq son kiriting: "))
+# if son%2:
+#     print("Raxmat")
+# else:
+#     print("Bu toq son emas.")
+
+# foydalanuvchi = int(input("Iltimos yoshingizni kiriting: "))
+# if foydalanuvchi <=4 or foydalanuvchi >=60:
+#     narh = 0
+# elif foydalanuvchi <=18:
+#     narh = 10000
+# else:
+#     narh = 20000
+# print(f"Sizga muzeyga kirish narhi {narh}-so'm")
+
+# x = int(input("Iltimos birinchi sonni kiriting: "))
+# y = int(input("Iltimos ikkinchi sonni kiriting: "))
+# if x==y:
+#     print(f"{x}={y}")
+# elif x>y:
+#     print(f"{x}>{y}")
+# else:
+#     print(f"{x}<{y}")
+
+# mahsulotlar = ['non', 'choy', 'banan', 'olma', 'yog', 'shakar', 'un', 'uzum']
+# savat = []
+# for n in range(5):
+#     savat.append(input(f"Savatga {n+1}-mahsulotni kiriting: "))
+    
+# for mahsulot in savat:
+#     if mahsulot in mahsulotlar:
+#         print(f"Do'konda {mahsulot} bor")
+#     else:
+#         print(f"Do'konda {mahsulot} yoq")
+
+# mahsulotlar = ['non', 'choy', 'banan', 'olma', 'yog', 'shakar', 'un', 'uzum']
+# savat = []
+# for n in range(5):
+#     savat.append(input(f"Savatga {n+1}-mahsulotni kiriting: "))
+    
+# bor_mahsulotlar = []
+# mavjud_emas = []   
+# for mahsulot in savat:
+#     if mahsulot in mahsulotlar:
+#         bor_mahsulotlar.append(mahsulot)
+#     else:
+#         mavjud_emas.append(mahsulot)
+        
+# if mavjud_emas:
+#    print("Dokonimizda quydagi mahsulotlar yoq.") 
+#    for mahsulot in mavjud_emas:
+#        print(mahsulot)
+#    else:
+#        print("Dokonimizda quydagi mahsulotlar bor.")
+
+# foydalanuvchilar = ['donyor', 'aziz', 'kamron', 'temur', 'sarvar']
+# login = input("Login kiriting: ")
+# if login.lower() in foydalanuvchilar:
+#    print("Kechirasiz bu login band, yangi login tanlang")
+# else:
+#    print("Hush kelibsiz, foydalanuvchi!")
+        
+# son = int(input("Birorta butun son kiriting: "))
+# for n in range(2,11):
+#     if not (son%n):
+#         print(f"{son} soni {n} ga qoldiqsiz bo'linadi")
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
